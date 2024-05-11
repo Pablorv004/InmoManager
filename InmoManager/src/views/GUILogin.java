9524e634a2728df76409acb507ab25069c8daae1
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2024-05-11 18:26:55 +0200

    Commit 11/05/2024
    - Creacion clase GUILogin vacía
    - Creacion clase GUIRegister

---
+++ InmoManager/src/views/GUILogin.java
@@ -0,0 +1,12 @@
+package views;
+
+import javax.swing.JFrame;
+
+public class GUILogin extends JFrame {
+
+	public GUILogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+	}
+
+}
---
+++ InmoManager/src/views/GUIRegister.java
@@ -0,0 +1,14 @@
+package views;
+
+import javax.swing.JFrame;
+
+public class GUIRegister extends JFrame {
+
+	public GUIRegister(GUILogin gLogin) {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setLocationRelativeTo()
+		
+	}
+
+}
