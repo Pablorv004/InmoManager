4f87391b404492e2217514c068878b30b1271c84
Pablorv28 <rodriguezp511@outlook.es>
2024-05-11 15:08:10 +0200

    Añadido la clase ConnectionDB.

---
+++ InmoManager/src/controllers/ConnectionDB.java
@@ -0,0 +1,29 @@
+package controllers;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+
+    private static Connection conn = null;
+
+    public static Connection connect() throws SQLException, ClassNotFoundException {
+        if (conn == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://158.179.219.75:3306/inmomanager?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "pestillo");
+            } catch (SQLException ex) {
+                throw new SQLException(ex);
+            } catch (ClassNotFoundException ex) {
+                throw new ClassCastException(ex.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void close() throws SQLException {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+}
