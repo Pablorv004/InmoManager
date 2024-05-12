3c643f842a86081e1a473ee4201ae1fd891ea387
Pablorv28 <rodriguezp511@outlook.es>
2024-05-12 19:56:46 +0200

    Fixed the issue where users can be checked on login.
    Note to mental self: fuck try-with unless you only use it once

--- InmoManager/src/controllers/ControllerLogin.java
+++ InmoManager/src/controllers/ControllerLogin.java
@@ -17,7 +17,7 @@ import views.GUIRegister;
 
 public class ControllerLogin {
 	private GUILogin login;
-	private int userAccessLvl;
+	private String userAccessLvl;
 
 	public ControllerLogin(GUILogin login) {
 		this.login = login;
@@ -28,16 +28,17 @@ public class ControllerLogin {
 	public boolean userFound(String username) {
 		System.out.println("Connecting...");
 		boolean userFound = false;
-		try (Connection conn = ConnectionDB.connect()) {
+		try {
+			Connection conn = ConnectionDB.connect();
 			System.out.println("Connected!");
 			// Checking clients...
 			PreparedStatement statementConnection = conn
 					.prepareStatement("SELECT userName FROM inmomanager.Clients WHERE userName = ?");
 			statementConnection.setString(1, username);
 			if (statementConnection.executeQuery().next()) {
-				
+
 				userFound = true;
-				userAccessLvl = 0;
+				userAccessLvl = "Clients";
 			} else {
 				// Checking managers...
 				statementConnection = conn
@@ -45,7 +46,7 @@ public class ControllerLogin {
 				statementConnection.setString(1, username);
 				if (statementConnection.executeQuery().next()) {
 					userFound = true;
-					userAccessLvl = 1;
+					userAccessLvl = "Managers";
 				} else {
 					// Checking administrators...
 					statementConnection = conn
@@ -53,7 +54,7 @@ public class ControllerLogin {
 					statementConnection.setString(1, username);
 					if (statementConnection.executeQuery().next()) {
 						userFound = true;
-						userAccessLvl = 2;
+						userAccessLvl = "Administrators";
 					}
 				}
 			}
@@ -67,39 +68,25 @@ public class ControllerLogin {
 		}
 		return userFound;
 	}
-	
-	public String getTableName() {
-		switch (userAccessLvl) {
-        case 0:
-            return "Clients";
-        case 1:
-            return "Managers";
-        case 2:
-            return "Administrators";
-        default:
-            System.out.println("This shouldn't happen. What user access level is this?");
-            break;
-    }
-		return null;
-	}
 
 	public boolean passwordMatches(String username, String password) {
-	    try (Connection conn = ConnectionDB.connect()) {
-	        // Let's determine which table to check:
-	        String tableName = getTableName();
-	        if(tableName != null) {
-	            String query = "SELECT userName FROM inmomanager." + tableName + " WHERE userName = ? AND password = ?";
-	            try (PreparedStatement statementConnection = conn.prepareStatement(query)) {
-	                statementConnection.setString(1, username);
-	                statementConnection.setString(2, password);
-	                ResultSet resultSet = statementConnection.executeQuery();
-	                return resultSet.next();
-	            }
-	        }
-	    } catch (ClassNotFoundException | SQLException e) {
-	        login.getLblError().setText("Contact an administrator for help. (Code: " + e.getClass().getSimpleName() + ", 1)");
-	    }
-	    return false;
+		try {
+			// Let's determine which table to check:
+			Connection conn = ConnectionDB.connect();
+			if (userAccessLvl != null) {
+				String query = "SELECT userName FROM inmomanager." + userAccessLvl + " WHERE userName = ? AND password = ?";
+				try (PreparedStatement statementConnection = conn.prepareStatement(query)) {
+					statementConnection.setString(1, username);
+					statementConnection.setString(2, password);
+					ResultSet resultSet = statementConnection.executeQuery();
+					return resultSet.next();
+				}
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			login.getLblError()
+					.setText("Contact an administrator for help. (Code: " + e.getClass().getSimpleName() + ", 1)");
+		}
+		return false;
 	}
 
 	private class ActListener implements ActionListener {
@@ -122,9 +109,9 @@ public class ControllerLogin {
 					login.getLblError().setText("Incorrect password.");
 				else {
 					switch (userAccessLvl) {
-					case 0 -> new GUIMainUser();
-					case 1 -> new GUIMainManager();
-					case 2 -> new GUIMainAdmin();
+					case "Clients" -> new GUIMainUser();
+					case "Managers" -> new GUIMainManager();
+					case "Administrators" -> new GUIMainAdmin();
 					default -> System.out.println("What access level is this?");
 					}
 					login.dispose();
