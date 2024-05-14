496e434f6493bf8577f400f2bc8125a7d8abcd26
Pablorv28 <rodriguezp511@outlook.es>
2024-05-14 09:27:41 +0200

    Fixed static methods for GlobalResources.

--- InmoManager/src/util/GlobalResources.java
+++ InmoManager/src/util/GlobalResources.java
@@ -4,18 +4,18 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class GlobalResources {
-	private Icon iconSmall;
+	private static Icon iconSmall;
 
 	public GlobalResources() {
 		iconSmall = new ImageIcon("files/images/InmoManagerSmol.png");
 	}
 
-	public Icon getIconSmall() {
+	public static Icon getIconSmall() {
 		return iconSmall;
 	}
 
-	public void setIconSmall(Icon iconSmall) {
-		this.iconSmall = iconSmall;
+	public static void setIconSmall(Icon iconSmall) {
+		GlobalResources.iconSmall = iconSmall;
 	}
 	
 	
