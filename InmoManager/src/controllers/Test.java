75b45e19e200a801bea0d12b0b4a67d2269c4a17
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2024-05-14 11:47:00 +0200

    Unused Test classes
    Removed unused test classes. The only one that exists now is in the "dummy" package

--- InmoManager/src/controllers/Test.java
+++
@@ -1,7 +0,0 @@
-package controllers;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- InmoManager/src/dummy/Test.java
+++ InmoManager/src/dummy/Test.java
@@ -1,7 +1,9 @@
 package dummy;
 
+import views.GUILogin;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    	new GUILogin();
     }
 }
